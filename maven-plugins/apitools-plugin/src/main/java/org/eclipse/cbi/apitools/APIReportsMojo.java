ba9b022ae18543de790e72477ebe5c4da2ea340b
Mickael Istria <mistria@redhat.com>
2013-01-24 11:11:05 +0100

    Fix dependency management

--- maven-plugins/apitools-plugin/src/main/java/org/eclipse/cbi/apitools/APIReportsMojo.java
+++ maven-plugins/apitools-plugin/src/main/java/org/eclipse/cbi/apitools/APIReportsMojo.java
@@ -10,6 +10,8 @@
 package org.eclipse.cbi.apitools;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Map;
 
 import net.oxbeef.apitools.core.ant.slim.ApiAnalysisTask;
@@ -19,8 +21,7 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.util.FileUtils;
-import org.eclipse.tycho.core.ArtifactDependencyVisitor;
-import org.eclipse.tycho.core.PluginDescription;
+import org.eclipse.tycho.ArtifactDescriptor;
 import org.eclipse.tycho.core.TychoProject;
 
 /**
@@ -58,17 +59,40 @@ public class APIReportsMojo extends AbstractMojo {
     private Map<String, TychoProject> projectTypes;
 
     private File profileLocation;
+    private File includeList;
 
     public void execute() throws MojoExecutionException, MojoFailureException {
     	ApiAnalysisTask task = new ApiAnalysisTask();
     	task.setBaseline(this.baseline.getAbsolutePath());
     	task.setDebug(getLog().isDebugEnabled());
     	task.setReport(this.outputDirectory.getAbsolutePath());
+    	populateIncludeList();
+    	task.setIncludeList(this.includeList.getAbsolutePath());
+    	task.setFilters(this.project.getArtifactId());
     	prepareProfile();
     	task.setProfile(this.profileLocation.getAbsolutePath());
     	// TODO includes & excludes
     	task.execute();
     }
+
+	private void populateIncludeList() throws MojoExecutionException {
+		StringBuilder content = new StringBuilder();
+		if (this.project.getPackaging().equals("eclipse-plugin") || this.project.getPackaging().equals("eclipse-test-plugin")) {
+			content.append(this.project.getArtifactId());
+		}
+		try {
+			this.outputDirectory.mkdirs();
+	    	this.includeList = new File(this.outputDirectory, "includeList");
+	    	if (!this.includeList.exists()) {
+	    		this.includeList.createNewFile();
+	    	}
+			FileOutputStream includeListStream = new FileOutputStream(this.includeList);
+			includeListStream.write(content.toString().getBytes());
+			includeListStream.close();
+		} catch (IOException ex) {
+			throw new MojoExecutionException("Could not write includeList", ex);
+		}
+	}
 
 	private void prepareProfile() throws MojoExecutionException, MojoFailureException {
 		this.profileLocation = new File(this.outputDirectory, "profile");
@@ -81,26 +105,31 @@ public class APIReportsMojo extends AbstractMojo {
 			this.profileLocation.mkdirs();
 		}
 		final StringBuilder errorBuilder = new StringBuilder();
-		tychoProject.getDependencyWalker(this.project).walk(new ArtifactDependencyVisitor() {
-			@Override
-			public void visitPlugin(PluginDescription pluginRef) {
-				try {
-					File location = pluginRef.getLocation();
-					if (location.isFile()) {
-						FileUtils.copyFileToDirectory(pluginRef.getLocation(), APIReportsMojo.this.profileLocation);
-					} else if (location.isDirectory()) {
-						if (pluginRef.getMavenProject() != null) {
-							getLog().warn("Reactor projects not yet supported: " + pluginRef.getMavenProject());
-						} else {
-							getLog().warn("Directory-shaped bundles not yet supported: " + pluginRef.getLocation());
-						}
+		for (ArtifactDescriptor dep : tychoProject.getDependencyArtifacts(project).getArtifacts()) {
+			try {
+				File location = dep.getLocation();
+				if (location.isFile()) {
+					FileUtils.copyFileToDirectory(dep.getLocation(), APIReportsMojo.this.profileLocation);
+				} else if (location.isDirectory()) {
+					if (dep.getMavenProject() != null) {
+						FileUtils.copyFileToDirectory(dep.getMavenProject().getArtifact().getAbsoluteFile(), APIReportsMojo.this.profileLocation);
+					} else {
+						getLog().warn("Directory-shaped bundles not yet supported: " + dep.getLocation());
 					}
-				} catch (Exception ex) {
-					errorBuilder.append("Couldn't copy " + pluginRef.getLocation() + " to " + APIReportsMojo.this.profileLocation);
-					errorBuilder.append("\n");
 				}
+			} catch (Exception ex) {
+				errorBuilder.append("Couldn't copy " + dep.getLocation() + " to " + APIReportsMojo.this.profileLocation);
+				errorBuilder.append("\n");
 			}
-		});
+		}
+		if (this.project.getPackaging().equals("eclipse-plugin") || this.project.equals("eclipse-test-plugin")) {
+			try {
+				FileUtils.copyFileToDirectory(this.project.getArtifact().getFile(), APIReportsMojo.this.profileLocation);
+			} catch (Exception ex) {
+				errorBuilder.append("Couldn't copy current project artifact (" + this.project.getArtifact().getFile() + ") to " + APIReportsMojo.this.profileLocation);
+				errorBuilder.append("\n");
+			}
+		}
 		if (errorBuilder.length() > 0) {
 			throw new MojoFailureException(errorBuilder.toString());
 		}
